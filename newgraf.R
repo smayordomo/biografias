d6760d3ba4908d72c2d8a396d85a1bcadbcbb5be
Salvador Mayordomo <smayordomo@gmail.com>
2024-05-30 00:08:25 +0200

    Nuevo programa gráfico

---
+++ newgraf.R
@@ -0,0 +1,20 @@
+# Cargamos los datos
+data(mtcars)
+attach(mtcars)
+
+# Tabla de frecuencias
+mi_tabla <- table(cyl)
+mi_tabla
+
+# Una fila, dos columnas
+par(mfrow = c(1, 2))
+
+# Gráfico de barras de frecuencia absoluta
+barplot(mi_tabla, main = "Frequencia absoluta",
+        col = rainbow(3))
+
+# Gráfico de barras de frecuencia relativa
+barplot(prop.table(mi_tabla) * 100, main = "Frequencia relativa (%)",
+        col = rainbow(3))
+
+par(mfrow = c(1, 1))
